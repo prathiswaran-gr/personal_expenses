41720cd792ff253a50c11f4928b5fd6abab7d67f
Prathis2003 <prathis2003@gmail.com>
2022-02-22 11:35:21 +0530

    clean code

--- lib/main.dart
+++ lib/main.dart
@@ -14,36 +14,30 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   Map<String, Map<String, dynamic>> expense = {
-    'Mobile': {'name': 'Mobile', 'price': 9999, 'date': DateTime(2021, 2, 14)},
-    'Note Books': {
-      'name': 'Note Books',
-      'price': 99,
-      'date': DateTime(2021, 2, 15)
-    },
-    'Stationary ': {
-      'name': 'Stationary items',
-      'price': 250,
-      'date': DateTime(2022, 1, 31)
-    },
-    'Bag': {'name': 'Bag', 'price': 499, 'date': DateTime(2022, 2, 18)},
-    'Post covers': {
-      'name': 'Post covers',
-      'price': 10,
-      'date': DateTime(2022, 1, 04)
-    },
-    'A4 Sheets': {
-      'name': 'A4 Sheets',
-      'price': 249,
-      'date': DateTime(2022, 1, 23)
-    },
-    'Watch': {'name': 'Watch', 'price': 99, 'date': DateTime(2022, 1, 11)},
-    'Shirts': {'name': 'Shirts', 'price': 412, 'date': DateTime(2022, 1, 21)},
-    'Sun Glass': {
-      'name': 'Sun Glass',
-      'price': 80,
-      'date': DateTime(2022, 1, 1)
-    }
+    'Mobile': {'price': 9999, 'date': DateTime(2021, 2, 14)},
+    'Note Books': {'price': 99, 'date': DateTime(2021, 2, 15)},
+    'Stationary ': {'price': 250, 'date': DateTime(2022, 1, 31)},
+    'Bag': {'price': 499, 'date': DateTime(2022, 2, 18)},
+    'Post covers': {'price': 10, 'date': DateTime(2022, 1, 04)},
+    'A4 Sheets': {'price': 249, 'date': DateTime(2022, 1, 23)},
+    'Watch': {'price': 99, 'date': DateTime(2022, 1, 11)},
+    'Shirts': {'price': 412, 'date': DateTime(2022, 1, 21)},
+    'Sun Glass': {'price': 80, 'date': DateTime(2022, 1, 1)}
   };
+
+  List<String> getSortedList() {
+    if (dropdownValue == 'entry') {
+      return expense.keys.toList();
+    } else if (dropdownValue == 'name') {
+      return (expense.keys.toList()..sort());
+    } else {
+      return expense.keys.toList()
+        ..sort(
+          (k1, k2) => (expense[k1]![dropdownValue])
+              .compareTo((expense[k2]![dropdownValue])),
+        );
+    }
+  }
 
   List<String> dropdownOptions = ['entry', 'name', 'price', 'date'];
   String dropdownValue = 'entry';
@@ -52,100 +46,70 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Personal Expenses'),
-            backgroundColor: Colors.purple,
-            elevation: 0.0,
-            actions: [
-              DropdownButtonHideUnderline(
-                child: DropdownButton(
-                  value: dropdownValue,
-                  items: dropdownOptions.map((value) {
-                    return DropdownMenuItem(
-                      value: value,
-                      child: Text(
-                        value,
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    );
-                  }).toList(),
-                  icon: const Icon(Icons.filter_list_sharp),
-                  onChanged: (String? value) {
-                    setState(() {
-                      dropdownValue = value!;
-                    });
-                  },
-                  iconEnabledColor: Colors.white,
-                  dropdownColor: Colors.purple,
+        appBar: AppBar(
+          title: const Text('Personal Expenses'),
+          backgroundColor: Colors.purple,
+          elevation: 0.0,
+          actions: [
+            DropdownButtonHideUnderline(
+              child: DropdownButton(
+                value: dropdownValue,
+                items: dropdownOptions.map((value) {
+                  return DropdownMenuItem(
+                    value: value,
+                    child: Text(
+                      value,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  );
+                }).toList(),
+                icon: const Icon(Icons.filter_list_sharp),
+                onChanged: (String? value) {
+                  setState(() {
+                    dropdownValue = value!;
+                  });
+                },
+                iconEnabledColor: Colors.white,
+                dropdownColor: Colors.purple,
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.amber,
+          child: const Icon(
+            Icons.add,
+            color: Colors.black,
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        body: Column(
+          children: [
+            const BarChart(),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: getSortedList()
+                      .map(
+                        (key) => ExpenseCard(
+                          item: key,
+                          price: expense[key]!['price'],
+                          date: expense[key]!['date'],
+                          deleteFunction: () {
+                            setState(() {
+                              expense.remove(key);
+                            });
+                          },
+                        ),
+                      )
+                      .toList(),
                 ),
               ),
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            backgroundColor: Colors.amber,
-            child: const Icon(
-              Icons.add,
-              color: Colors.black,
             ),
-          ),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerFloat,
-          body: dropdownValue != 'entry'
-              ? Column(
-                  children: [
-                    const BarChart(),
-                    Expanded(
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: (expense.keys.toList()
-                                ..sort(
-                                  (k1, k2) => (expense[k1]![dropdownValue])
-                                      .compareTo((expense[k2]![dropdownValue])),
-                                ))
-                              .map(
-                                (key) => ExpenseCard(
-                                  item: key,
-                                  price: expense[key]!['price'],
-                                  date: expense[key]!['date'],
-                                  deleteFunction: () {
-                                    setState(() {
-                                      expense.remove(key);
-                                    });
-                                  },
-                                ),
-                              )
-                              .toList(),
-                        ),
-                      ),
-                    ),
-                  ],
-                )
-              : Column(
-                  children: [
-                    const BarChart(),
-                    Expanded(
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: expense.keys
-                              .map(
-                                (key) => ExpenseCard(
-                                  item: key,
-                                  price: expense[key]!['price'],
-                                  date: expense[key]!['date'],
-                                  deleteFunction: () {
-                                    setState(() {
-                                      expense.remove(key);
-                                    });
-                                  },
-                                ),
-                              )
-                              .toList(),
-                        ),
-                      ),
-                    ),
-                  ],
-                )),
+          ],
+        ),
+      ),
     );
   }
 }
